e9d47b1c4ba5188c5e57f1c1d0387966a6e2c953
VannieJay Web Services <info.vanniejay@gmail.com>
2026-02-05 20:47:23 +0100

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,11 +5,12 @@ import typography from "@tailwindcss/typography";
 export default {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-  ],
+  "./index.html",
+  "./pages/**/*.{ts,tsx}",
+  "./components/**/*.{ts,tsx}",
+  "./app/**/*.{ts,tsx}",
+  "./src/**/*.{ts,tsx}",
+],
   prefix: "",
   theme: {
     container: {
